6aee517161334eea26df641f62a41887dd49ab9e
Patrik Drab <patrikdrab75@gmail.com>
2025-03-26 08:30:09 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UIKitPro",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "UIKitPro",
+            targets: ["UIKitPro"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "UIKitPro"),
+
+    ]
+)
---
+++ README.md
@@ -0,0 +1,9 @@
+//
+//
+//
+// Created by: Patrik Drab on 26/03/2025
+// Copyright (c) 2025 UIKitPro 
+//
+//         
+
+
